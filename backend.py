b75e542642841ddb69a0ad37efa5c322f93a9781
souhelc706-boop <souhelc706@gmail.com>
2025-11-24 13:34:02 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,4 +1,11 @@
 from flask import Flask
+import os
+
 app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return "Backend funcionando!"
+
 if _name_ == "_main_":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
