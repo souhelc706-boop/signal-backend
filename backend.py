cf21778e2f1b3363cb75d5ff9cd8cf767c9912c7
souhelc706-boop <souhelc706@gmail.com>
2025-11-24 13:39:35 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,19 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(_name_)
 CORS(app)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def home():
-    return jsonify({"message": "Backend funcionando correctamente en Render!"})
-
-@app.route("/sumar", methods=["POST"])
-def sumar():
-    data = request.get_json()
-    a = data.get("a", 0)
-    b = data.get("b", 0)
-    return jsonify({"resultado": a + b})
+    return jsonify({"status": "Backend funcionando correctamente 🎉"})
 
 if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=5000)
