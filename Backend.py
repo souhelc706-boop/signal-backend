ddb9e18183a829ae4b42050e2e8092f051a7b5c3
souhelc706-boop <souhelc706@gmail.com>
2025-11-24 13:31:10 -0400

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -1,13 +1,4 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-import os
-
+from flask import Flask
 app = Flask(_name_)
-CORS(app)
-
-@app.route("/")
-def home():
-    return jsonify({"status": "Backend funcionando correctamente"})
-
 if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
